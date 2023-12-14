43bc7b2ba8b8ee90d0021ba6d3c7c55670f651a0
jsh9057 <jsh9057@skuniv.ac.kr>
2023-12-14 21:29:06 +0900

    카카오 키워드 장소 검색 api 적용

--- src/main/java/com/example/project/api/dto/DocumentDto.java
+++ src/main/java/com/example/project/api/dto/DocumentDto.java
@@ -11,19 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentDto {
-    /*
-    address_name	String	전체 지번 주소 또는 전체 도로명 주소, 입력에 따라 결정됨
-    address_type	String	address_name의 값의 타입(Type)
-                            다음 중 하나:
-                            REGION(지명)
-                            ROAD(도로명)
-                            REGION_ADDR(지번 주소)
-                            ROAD_ADDR(도로명 주소)
-    x	String	X 좌표값, 경위도인 경우 경도(longitude)
-    y	String	Y 좌표값, 경위도인 경우 위도(latitude)
-    address	Address	지번 주소 상세 정보
-    road_address	RoadAddress	도로명 주소 상세 정보
-     */
+
+    @JsonProperty("place_name")
+    private String placeName;
+
     @JsonProperty("address_name")
     private String addressName;
 
@@ -33,4 +24,6 @@ public class DocumentDto {
     @JsonProperty("x")
     private double longitude;
 
+    @JsonProperty("distance")
+    private double distance;
 }
--- src/main/java/com/example/project/direction/service/DirectionService.java
+++ src/main/java/com/example/project/direction/service/DirectionService.java
@@ -1,6 +1,7 @@
 package com.example.project.direction.service;
 
 import com.example.project.api.dto.DocumentDto;
+import com.example.project.api.service.KakaoCategorySearchService;
 import com.example.project.direction.entity.Direction;
 import com.example.project.direction.repository.DirectionRepository;
 import com.example.project.pharmacy.service.PharmacySearchService;
@@ -25,6 +26,7 @@ public class DirectionService {
 
     private final PharmacySearchService pharmacySearchService;
     private final DirectionRepository directionRepository;
+    private final KakaoCategorySearchService kakaoCategorySearchService;
 
     public List<Direction> saveAll(List<Direction> directionList) {
         if(CollectionUtils.isEmpty(directionList)) return Collections.emptyList();
@@ -55,6 +57,28 @@ public class DirectionService {
                 .limit(MAX_SEARCH_COUNT)
                 .collect(Collectors.toList());
 
+    }
+
+    // pharmacy search by category kakao api
+    public List<Direction> buildDirectionListByCategoryApi(DocumentDto inputDocumentDto) {
+        if(Objects.isNull(inputDocumentDto)) return Collections.emptyList();
+
+        return kakaoCategorySearchService
+                .requestPharmacyCategorySearch(inputDocumentDto.getLatitude(), inputDocumentDto.getLongitude(), RADIUS_KM)
+                .getDocumentList()
+                .stream().map(resultDocumentDto ->
+                        Direction.builder()
+                                .inputAddress(inputDocumentDto.getAddressName())
+                                .inputLatitude(inputDocumentDto.getLatitude())
+                                .inputLongitude(inputDocumentDto.getLongitude())
+                                .targetPharmacyName(resultDocumentDto.getPlaceName())
+                                .targetAddress(resultDocumentDto.getAddressName())
+                                .targetLatitude(resultDocumentDto.getLatitude())
+                                .targetLongitude(resultDocumentDto.getLongitude())
+                                .distance(resultDocumentDto.getDistance() * 0.001)
+                                .build())
+                .limit(MAX_SEARCH_COUNT)
+                .collect(Collectors.toList());
     }
 
     // Haversine formula
